67677879a3d6956ca8ed9e075baac3223b428add
Harezlak Robert <harezlak.robert7@gmail.com>
2018-07-05 01:50:01 +0200

    Add rating field to Movie

--- RazorPagesMovies/Migrations/20180704234853_Rating.Designer.cs
+++ RazorPagesMovies/Migrations/20180704234853_Rating.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace RazorPagesMovies.Migrations
 {
     [DbContext(typeof(MovieContext))]
-    partial class MovieContextModelSnapshot : ModelSnapshot
+    [Migration("20180704234853_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
